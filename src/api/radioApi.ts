db737e3b39f41df3eb74a7a6985e1b69a808469c
Alessandro1918 <alessandro1918@hotmail.com>
2023-02-16 18:21:32 -0300

    refactor: rebuild old code as Next app

---
+++ src/api/radioApi.ts
@@ -0,0 +1,6 @@
+const axios = require('axios')
+
+export const radioApi = axios.create({
+    //baseURL: 'http://localhost:5001/radioapi-b7bc5/us-central1/radioAPI'        //dev
+    baseURL: 'https://us-central1-radioapi-b7bc5.cloudfunctions.net/radioAPI' //prod
+})
