5cdbcad30e423987fd60711fc33bc42f227379df
Alessandro1918 <alessandro1918@hotmail.com>
2023-02-16 17:04:59 -0300

    feat: init project: create-next-app

--- src/services/api.ts
+++
@@ -1,7 +0,0 @@
-const axios = require('axios')
-
-export const api = axios.create({
-    //baseURL: 'http://localhost:5001/radioapi-b7bc5/us-central1/radioAPI'        //dev
-    baseURL: 'https://us-central1-radioapi-b7bc5.cloudfunctions.net/radioAPI' //prod
-})
-
